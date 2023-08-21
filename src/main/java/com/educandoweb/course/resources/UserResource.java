607692195e20834c55f8c47823f46b53a0dad9ca
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-21 12:48:30 -0300

    User update

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,17 +37,23 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@PostMapping
-	public ResponseEntity<User> inser(@RequestBody User obj){  //@requestBody TRANSFORMA O OBJETO JSON A OBJETO USER
+	@PostMapping  												 // ANOTAÇÃO PARA INSERIR UTILIZANDO O PADRÃO REST
+	public ResponseEntity<User> inser(@RequestBody User obj){   //@requestBody TRANSFORMA O OBJETO JSON A OBJETO USER
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); // METODO QUE MONTA A URL DO NOVO USUARIO E RETORNA PRA URI
 		return ResponseEntity.created(uri).body(obj);
 	}
 	
-	@DeleteMapping(value = "/{id}")  
+	@DeleteMapping(value = "/{id}") 						      // ANOTAÇÃO PARA DELETAR PELO PADRÃO REST
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@PutMapping(value = "/{id}")								  // ANOTAÇÃO PARA ATUALIZAÇÃO PELO PADRÃO REST
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -32,4 +32,17 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id); // PEGA UM OBJ E DEIXA DISPONIVEL PRA VC ALTERAR, SEM QUE ALTERE ELE NO BD // PEGA UMA REFERENCIA
+		updateDate(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateDate(User entity, User obj) { // METODO PARA ATUALIZAR OS DADOS
+		entity.setName(obj.getName());               // LANÇA O NOME NO SETNAME -->> BASEADO NO NOME QUE RECEBEU DO OBJ
+		entity.setEmail(obj.getEmail());			 // LANÇA O NOME NO SETEMAIL -->> BASEADO NO NOME QUE RECEBEU DO OBJ
+		entity.setPhone(obj.getPhone());		     // LANÇA O NOME NO SETPHONE -->> BASEADO NO NOME QUE RECEBEU DO OBJ
+		
+	}
 }
